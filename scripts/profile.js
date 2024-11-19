09dad56581683d303dd0f0e9aff8b9e0963f5333
Wonjun Jung <kokoseij@gmail.com>
2024-11-18 23:27:57 -0800

    profile: remove gravatar functions and import them from gravatar.js

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,5 +1,3 @@
-const GRAVATAR_BASEURL = "https://gravatar.com/avatar/"
-
 let pfp = document.getElementById("hopinFormPfp")
 let nameLabel = document.getElementById("hopinFormNameLabel")
 let nameInput = document.getElementById("hopinFormUserName")
@@ -20,39 +18,12 @@ function submitCallback(e) {
     });
 }
 
-function calculateEmailHash(text) {
-    // TODO: If adding sha256 impl, add check to enable polyfill
-    const data = (new TextEncoder()).encode(text);
-    const hash = window.crypto.subtle.digest(
-        "SHA-256", data
-    ).then(hashdata => {
-        const hashstr = Array.from(new Uint8Array(hashdata))
-                             .map(n=>n.toString(16).padStart(2, "0"))
-                             .join("");
-        console.log({"email_hash": hashstr});
-        return hashstr;
-    })
-
-    // This is promise!
-    return hash;
-}
-
-async function getAvatarUrlFromEmail(email) {
-    if (window.crypto.subtle === null) {
-        // This should be here on all modern browsers
-        // TODO: Replace this with proper polyfill
-        return "https://http.cat/images/404.jpg"
-    }
-
-    const hash = await calculateEmailHash(email);
-
-    return GRAVATAR_BASEURL + hash + "?s=256&d=retro";
-}
-
 function setProfilePic(email) {
-    getAvatarUrlFromEmail(email).then(url => {
-        pfp.src = url
-    });
+    import("./gravatar.js").then(({getAvatarUrlFromEmail})=>{
+        getAvatarUrlFromEmail(email).then(url => {
+            pfp.src = url
+        });
+    })
 }
 
 function populateFormInfo(data) {
